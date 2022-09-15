e3f4399438b549015f71ef91e5fbb27eeb9c0335
NinjaGo31 <barryblast31@outlook.com>
2022-09-14 22:05:36 -0400

    Edited comment

--- fix_me.py
+++ fix_me.py
@@ -1,4 +1,7 @@
 # 2 + 2 = ?
 # Print the answer right below this line!
 print(2+2)
-# Bababooey
+# The FitnessGram PACER Test is a multistage aerobic capacity test that progressively gets more difficult as it continues.
+# The test is used to measure a student's aerobic capacity as part of the FitnessGram assessment.
+# Students run back and forth as many times as they can, each lap signaled by a beep sound.
+# The test get progressively faster as it continues until the student reaches their max lap score.
